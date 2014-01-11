d7835f3c9a0e6309afa400515a2409ce34567047
Brad Pauly <hi@bradpauly.com>
2014-01-11 15:49:55 -0800

    Start building a login form.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 RegisterAndLogin::Application.routes.draw do
   resources :registrations, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
   root 'registrations#new'
 end
